9cebce8c08d929b866ed6e263911fafdac4cdbc3
Henrique Eustáquio Lopes Ferreira <elfo@MacBook-Air-de-Henrique.local>
2018-05-21 23:26:22 -0300

    API com questao 1 pronta, 2 em andamento (debaixo dos comentarios)

---
+++ api.py
@@ -0,0 +1,146 @@
+""" 
+API para trabalho de Engenharia de Software 2
+
+Autores: 
+	Henrique Eustaquio Lopes Ferreira 2015068990
+	Tiago Melo Tannus 
+
+Implementado com:
+	Flask 1.0.2
+	Python 2.7.10 (default, Jul 15 2017, 17:16:57)
+	[GCC 4.2.1 Compatible Apple LLVM 9.0.0 (clang-900.0.31)]
+"""
+
+from flask import Flask, jsonify, abort, make_response, request
+import os
+
+app = Flask(__name__)
+
+tasks = [
+	{
+		'id': 1,
+		'title': u'Buy groceries',
+		'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+		'done': False 
+	},
+	{
+		'id': 2,
+		'title': u'Learn Python',
+		'description': u'Need to find a good Python tutorial on the web',
+		'done': False 
+	}
+]
+
+# Numero de publicacoes em uma determinada conferencia de uma area
+@app.route("/api/1")
+def get_publi_conf_area():
+	conf = request.args.get('conf')
+   	area = request.args.get('area')
+
+   	if len(conf)==0 or len(area)==0:
+   		abort(404)
+   	
+   	file = ""
+   	file_path = "data/"
+
+   	for root, dirs, files in os.walk("data"):
+   		for filename in files:
+   			if filename == (area + "-out-confs.csv"):
+   				file = filename
+
+   	data = open(file_path + file, "r")
+
+   	n = ""
+   	correct = ""
+   	what = []
+   	for line in data.readlines():
+		what.append(line)
+		if line.find(conf) != -1:
+ 			correct = line
+   	
+   	return correct[correct.find(",")+1:len(correct)-1]
+
+# Número de publicações no conjunto de conferências de uma área
+@app.route("/api/2")
+def get_publi_area():
+	conf = request.args.get('area')
+
+   	if len(conf)==0 or len(area)==0:
+   		abort(404)
+   	
+   	file = ""
+   	file_path = "data/"
+
+   	for root, dirs, files in os.walk("data"):
+   		for filename in files:
+   			if filename == (area + "-out-confs.csv"):
+   				file = filename
+
+   	data = open(file_path + file, "r")
+
+   	n = 0
+   	correct = ""
+   	what = []
+   	for line in data.readlines():
+		n += line[line.find(",")+1:len(line)-1]
+   	
+   	return 
+
+
+
+@app.route("/todo/api/v1.0/tasks", methods=['GET'])
+def get_tasks():
+	return jsonify({'tasks': tasks})
+
+@app.route("/todo/api/v1.0/tasks/<int:task_id>", methods=['GET'])
+def get_task(task_id):
+	task = [task for task in tasks if task['id'] == task_id]
+	if len(task) == 0:
+		abort(404)
+	return jsonify({'task': task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+	return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route("/todo/api/v1.0/tasks", methods=['POST'])
+def create_task():
+	if not request.json or not 'title' in request.json:
+		abort(400)
+	task = {
+		'id': tasks[-1]['id'] + 1,
+		'title': request.json['title'],
+		'description': request.json.get('description', ""),
+		'done': False
+	}
+	tasks.append(task)
+	return jsonify({'task': task}), 201
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+    
+if __name__ == '__main__':
+	app.run(debug=True)
